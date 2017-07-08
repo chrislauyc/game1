bbf93fd56e125e72e0d99172575aaf353c5196dd
joshua <joshua.c4mpbell@gmail.com>
2017-07-08 17:57:25 -0600

    Created score on the GUI

--- GUI.py
+++ GUI.py
@@ -1,24 +1,37 @@
-import pygame
-class GUI:
-	def __init__():
-		pass
-	def run_GUI():
-		pygame.init()
-		screen = pygame.display.set_mode((805,658))
-		done = False
+import pygame, sys
+from pygame.locals import *
 
-		img = pygame.image.load('Chutes&Ladders1.gif')
-		img = pygame.transform.scale(img, (650,658))
-		while not done:
-			screen.blit(img, (0,0))
-			for event in pygame.event.get():
-				
-				if event.type == pygame.QUIT:
-					done = True
-				pygame.display.flip()
+class GUI:
+	def __init__(self):
+		pass
+	def run_GUI(self):
+            pygame.init()
+            size = width, height = 805,658
+            screen = pygame.display.set_mode(size)
+            pygame.display.set_caption("testing")
+            myfont = pygame.font.SysFont("monospace", 16)
+            BLACK = (000,000,000)
+            score = 0
+            done = False
+            img = pygame.image.load('Chutes&Ladders1.gif')
+            img = pygame.transform.scale(img, (650,658))
+            
+            while not done:
+                screen.blit(img, (0,0))
+                pygame.display.flip()
+                for event in pygame.event.get():
+                    # I remove the timer just for my testing
+                    if event.type == pygame.QUIT:
+                        done = True
+                        sys.exit()
+                screen.fill(BLACK)
+                scoretext = myfont.render("P1 score: {0}".format(score), 1, (50,250,0))#color
+                screen.blit(scoretext, (650, 0))
+                score += 1
+            
 	def display_dice():
 		#display dice 
-		
+			
 		#if click event, return true. else, return false
 		return
 
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import Node.py
+import Node, GUI
+def main():
+	# my code here
+	#menu
+	print "menu (please select an option)"
+	print "1-test gui"
+	print "2-quit"
+	num = raw_input('enter a number: ')
+	if num == '1':
+		gui = GUI.GUI()
+		gui.run_GUI()
+		print "im in 1"
+	elif num == '2':
+		print "im in 2"
+		quit()
 
-#implement menu
-
-
-#GUI
- #use the pygame library or something
+if __name__ == "__main__":
+	main()
 
